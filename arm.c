38533162d9860a6ba8a2d73f68ef8603a4c35951
pdurgaprasad <49471166+pdurgaprasad@users.noreply.github.com>
2019-04-10 17:22:27 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n=56, reversedNumber = 0, remainder;
+
+    while(n != 0)
+    {
+        remainder = n%10;
+        reversedNumber = reversedNumber*10 + remainder;
+        n /= 10;
+    }
+
+    printf("Reversed Number = %d", reversedNumber);
+
+    return 0;
+}
